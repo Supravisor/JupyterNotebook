892cddfd019422ab3ed919e978c8a4b7a750a48f
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-01-15 17:21:21 +1300

    add data function

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 // load data
 let variable = document.getElementById("variable");
 let loadData = document.getElementById("load-data");
+
+const data = (stat) => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'Load data' section");
+  } else {
+      document.editor.textbox.value+= '\n' + document.editor.variable.value + '.' + stat;
+  }
+}
