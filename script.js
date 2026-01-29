7141798b51954fff5d75f945cb818d39ffbd9bcc
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-01-29 17:07:41 +1300

    modify loadData variable

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 
 // Load data
 let variable = document.getElementById("variable");
-let loadData = document.getElementById("load-data");
+let loadData = `
+conn = sqlite3.connect('data/sakila.db')
+
+df = pd.read_sql('''
+    SELECT
+        rental.rental_id, rental.rental_date, rental.return_date,
+        customer.last_name AS customer_lastname,
+        store.store_id,
+        city.city AS rental_store_city,
+        film.title AS film_title, film.rental_duration AS film_rental_duration,
+        film.rental_rate AS film_rental_rate, film.replacement_cost AS film_replacement_cost,
+        film.rating AS film_rating
+    FROM rental
+    INNER JOIN customer ON rental.customer_id == customer.customer_id
+    INNER JOIN inventory ON rental.inventory_id == inventory.inventory_id
+    INNER JOIN store ON inventory.store_id == store.store_id
+    INNER JOIN address ON store.address_id == address.address_id
+    INNER JOIN city ON address.city_id == city.city_id
+    INNER JOIN film ON inventory.film_id == film.film_id
+    ;
+''', conn, index_col='rental_id', parse_dates=['rental_date', 'return_date'])
+`;
 
 const data = (stat) => {
   if (variable.value === "") {
